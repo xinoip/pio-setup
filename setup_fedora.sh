b5e43e471a951d0e513748c5a2af23628dc4f1aa
xinoip <pionix@protonmail.com>
2023-01-12 18:12:43 +0300

    new setup script

--- setup_fedora.sh
+++ setup_fedora.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-cd "$(dirname "$0")"
+cd `dirname "$0"`;
 
 ### CONFIGURATION
-# won't use `source` due to security concerns
 conf_home="/home/pio";
-conf_ssh_comment="ssh comment foo@foo.com";
 conf_dnf=`cat config/dnf.conf`;
 conf_xdg_dirs=`cat config/xdg_dirs.conf`;
 conf_zshrc=`cat config/zshrc.conf`;
-conf_alacritty=`cat alacritty_settings.yml`;
-conf_ohmyzsh_custom_dir="${conf_home}/.oh-my-zsh/custom";
+conf_p10k=`cat config/p10k.conf`;
 
 echo "Setup dnf.conf";
 sudo sh -c "echo \"${conf_dnf}\" >> /etc/dnf/dnf.conf";
@@ -24,7 +21,8 @@ mkdir ${conf_home}/pio-xdg ${conf_home}/download ${conf_home}/picture ${conf_hom
 echo "Updating stuff";
 sudo dnf update --refresh -y;
 echo "Download stuff";
-sudo dnf install git clang clang-tools-extra zsh xclip alacritty flameshot;
+sudo dnf install git clang clang-tools-extra zsh xclip flameshot tree;
+sudo dnf install fzf lsd;
 sudo dnf install dnf-plugins-core;
 
 echo "Get vscode";
@@ -46,15 +44,10 @@ cat ${conf_home}/.ssh/id_ed25519.pub | xclip -selection clipboard;
 echo "Created ssh key and copied it to clipboard";
 
 echo "= Setup zsh =";
-echo "Setup oh-my-zsh";
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-mkdir -p "${conf_ohmyzsh_custom_dir}";
-echo "Get zsh plugins";
-/usr/bin/git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $conf_ohmyzsh_custom_dir/plugins/zsh-syntax-highlighting;
-/usr/bin/git clone https://github.com/zsh-users/zsh-autosuggestions.git $conf_ohmyzsh_custom_dir/plugins/zsh-autosuggestions;
-/usr/bin/git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $conf_ohmyzsh_custom_dir/themes/powerlevel10k;
 echo "Setup .zshrc";
 echo "${conf_zshrc}" > ${conf_home}/.zshrc;
+echo "${conf_p10k} > ${conf_home}/.p10k.zsh;
+curl -L git.io/antigen > ${conf_home}/antigen.zsh
 
 echo "Download fonts for p10k";
 curl -LJO https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
@@ -70,15 +63,7 @@ sudo dnf search iosevka;
 sudo dnf install iosevka-fonts;
 fc-cache;
 
-echo "Setup alacritty config";
-mkdir -p ${conf_home}/.config/alacritty;
-echo ${conf_alacritty} > ${conf_home}/.config/alacritty/alacritty.yml;
 
-echo "Setup vcpkg";
-mkdir -p ${conf_home}/repo;
-git clone https://github.com/Microsoft/vcpkg.git ${conf_home}/repo/vcpkg;
-cd ${conf_home}/repo
-./vcpkg/bootstrap-vcpkg.sh
 
 echo -e "\n\n\n";
 echo "If we lived through all of it... here are the manual stuff to do!";
